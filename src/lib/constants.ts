b8f609b4558ec08f7ff3db2308bd9ac4ee4b9eef
Zachary Blake <hi@ztb.dev>
2025-12-08 15:41:39 -0800

    adding some constants for eas

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,3 +6,9 @@ export const STORAGE_KEYS = {
 } as const;
 
 export type StorageKey = (typeof STORAGE_KEYS)[keyof typeof STORAGE_KEYS];
+
+export const EAS_BASE_ADDRESS = "0x4200000000000000000000000000000000000021";
+export const EAS_BASE_SCHEMA_REGISTRY_ADDRESS =
+  "0x4200000000000000000000000000000000000020";
+export const EAS_PROJECT_SCHEMA =
+  "string name,string description,string website,address creator,uint256 createdAt";
