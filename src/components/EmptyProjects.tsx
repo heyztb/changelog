dee90d2282251d9cab8272ff04175b233250c17d
Zachary Blake <hi@ztb.dev>
2025-12-08 15:41:39 -0800

    working on onboarding flow a bit, this should be better

---
+++ src/components/EmptyProjects.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { IconFolderCode } from "@tabler/icons-react";
+import { ArrowUpRightIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { CreateProjectModal } from "./CreateProjectModal";
+
+export function EmptyProjects() {
+  const [createFormOpen, setCreateFormOpen] = useState(false);
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <IconFolderCode />
+        </EmptyMedia>
+        <EmptyTitle>No Projects Yet</EmptyTitle>
+        <EmptyDescription>
+          You haven&apos;t created any projects yet. Get started by creating
+          your first project.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <div className="flex gap-2">
+          <Button onClick={() => setCreateFormOpen(true)}>
+            Create Project
+          </Button>
+        </div>
+      </EmptyContent>
+      <Button
+        variant="link"
+        asChild
+        className="text-muted-foreground"
+        size="sm"
+      >
+        <a href="#">
+          Learn More <ArrowUpRightIcon />
+        </a>
+      </Button>
+      <CreateProjectModal
+        isOpen={createFormOpen}
+        onConfirm={() => setCreateFormOpen(false)}
+        onClose={() => setCreateFormOpen(false)}
+      />
+    </Empty>
+  );
+}
